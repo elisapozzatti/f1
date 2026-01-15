17c8850cf6b2fd535806ecdf2955fba0a12713d0
elisapozzatti <elisa.pozzatti@gmail.com>
2026-01-15 17:34:28 +0100

    aggiunta mappa

--- frontend/src/components/Circuitsmap.tsx
+++ frontend/src/components/Circuitsmap.tsx
@@ -1,7 +1,43 @@
+import { useEffect } from "react";
+import L from "leaflet";
 import "./Circuitsmap.css";
+import { circuits } from "./coordinates";
 
 function Circuitsmap() {
-  return <></>;
+  useEffect(() => {
+    // Inizializza la mappa
+    const map = L.map("map").setView([45.6196, 9.2811], 15); // Monza
+
+    // Aggiungi layer OpenStreetMap
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; OpenStreetMap contributors",
+    }).addTo(map);
+
+    // Aggiungi marker
+    L.marker([45.6196, 9.2811])
+      .addTo(map)
+      .bindPopup("Autodromo Nazionale Monza")
+      .openPopup();
+
+    // Aggiungo gli altri punti con il marker
+    circuits.forEach((circuit) => {
+      L.marker(circuit.coords).addTo(map).bindPopup(circuit.name);
+    });
+
+    // Cleanup della mappa quando il componente viene smontato
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="title">
+        <h3>MAPPA CON TUTTI I CIRCUITI DI F1</h3>
+      </div>
+      <div id="map"></div>
+    </>
+  );
 }
 
 export default Circuitsmap;
