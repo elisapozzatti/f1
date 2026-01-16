af4dd1f696ed400da3af7f2d41f5afa0137913d2
elisapozzatti <elisa.pozzatti@gmail.com>
2026-01-16 11:51:11 +0100

    .

--- node_modules/backend/src/route/circuits.ts
+++ node_modules/backend/src/route/circuits.ts
@@ -1,7 +1,20 @@
 import Router from "@koa/router";
+import circuitSchema from "../models/circuits.js";
 
 const router = new Router({
-  prefix: "/api/teamprincipals",
+  prefix: "/api/circuits",
+});
+
+//get tutti i circuiti
+router.get("/", async (ctx) => {
+  try {
+    const circuits = await circuitSchema.find();
+    ctx.body = circuits;
+    ctx.status = 200;
+  } catch (errore) {
+    ctx.status = 404;
+    ctx.body = { error: "Errore nel trovare i dati" };
+  }
 });
 
 export default router;
